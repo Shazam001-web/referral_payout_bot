71a5734a2a0ae2098ed948de020891528eeb2910
Shazam001-web <lordpopg001@gmail.com>
2026-01-05 02:55:15 +0100

    Create configure.py

---
+++ configure.py
@@ -0,0 +1,10 @@
+TOKEN = "8256893374:AAHHI21jTjvYWEH5kGXELEeN_ZqRb0SNqZ4"
+BOT_USERNAME = "Metacash_updatesbot"   # without @
+
+CHANNEL_USERNAME = "@tokitotechbackup"
+CHANNEL_LINK = "https://t.me/tokitotechbackup"
+
+ADMIN_USERNAME = "@shazamhacker"
+ADMIN_LINK = "https://t.me/shazamhacker"
+
+REQUIRED_REFERRALS = 8
